0d88cc228ef95ffc052a06512cf4efccb2a07f41
Palomi Nihalani <65126187+pnihalani26@users.noreply.github.com>
2025-03-18 20:12:32 -0400

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -29,8 +29,23 @@ else if (C.length == z) {
     console.log("good job!")
 }
     
-var L1 = ["Watermelon", "Pinapple", "Pear", "Banana"];
-var L2 = ["Apple", "Banana", "Kiwi", "Orange"];
+$(document).ready(function() {
+    $("#fullBio").hide();
+
+    $("#readMoreButton").click(function() {
+        if ($("#fullBio").is(":visible")) {
+            $("#fullBio").hide();
+            $("#shortBio").show();
+            $("#readMoreButton").text("Read More");
+        }
+        else {
+            $("#fullBio").show();
+                $("#readMoreButton").text("Read Less");
+            }
+    });
+});
+// var L1 = ["Watermelon", "Pinapple", "Pear", "Banana"];
+// var L2 = ["Apple", "Banana", "Kiwi", "Orange"];
 
 // function findTheBanana(array) {
 //     for (let i=0; i<array.length; i++) {
@@ -43,16 +58,16 @@ var L2 = ["Apple", "Banana", "Kiwi", "Orange"];
 // findTheBanana(L1);
 // findTheBanana(L2);
 
-function findTheBanana(array, arrayName) {
-    array.forEach(function(currentValue, index, arr) {
-        if (currentValue == "Banana") {
-        alert("We found a Banana in the " + arrayName + " array");
-        }
-    });
-}
+// function findTheBanana(array, arrayName) {
+//     array.forEach(function(currentValue, index, arr) {
+//         if (currentValue == "Banana") {
+//         alert("We found a Banana in the " + arrayName + " array");
+//         }
+//     });
+// }
 
-findTheBanana(L1, "first");
-findTheBanana(L2, "second");
+// findTheBanana(L1, "first");
+// findTheBanana(L2, "second");
 
 function greetingFunc() {
     const d = new Date();
@@ -73,3 +88,54 @@ function greetingFunc() {
 if (window.location.href.includes("index.html")) {
     greetingFunc();
 }
+
+function addYear() {
+    var d = new Date();
+    var y = d.getFullYear();
+    var E = document.getElementById("copyYear");
+    E.innerHTML += " " + y;
+}
+
+function showList() {
+    document.getElementById("myList").style.display = "block";
+    var showButton = document.getElementById("showButton");
+    if (showButton) {
+        showButton.style.display = "none";
+    }
+}
+
+
+function validateForm() {
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const validationMessage = document.getElementById("validationMessage");
+  
+    validationMessage.innerHTML = "";
+    validationMessage.className = "";
+  
+    if (!name.value) {
+      name.setCustomValidity("You forgot to enter your name!");
+    } else {
+      name.setCustomValidity(""); 
+    }
+      if (!email.value) {
+      email.setCustomValidity("We need your email address to contact you!");
+    } else {
+      email.setCustomValidity("");
+    }
+  
+    if (!name.checkValidity()) {
+        validationMessage.innerHTML = name.validationMessage;
+      return false;
+    }
+  
+    if (!email.checkValidity()) {
+        validationMessage.innerHTML = email.validationMessage;
+      return false;
+    }
+  
+    validationMessage.innerHTML = "Form submitted successfully!";
+    validationMessage.className = "success";
+    return false;
+  }
+
